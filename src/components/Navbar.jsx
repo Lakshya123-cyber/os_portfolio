aad8503ef4a5c8848a0630ae991cada251ed1880
Lakshya123-cyber <lakshyaraikwal85@gmail.com>
2025-11-22 21:11:44 +0200

    first

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import dayjs from "dayjs";
+import { navIcons, navLinks } from "@constants";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold">Raikwal's Portfolio</p>
+
+        <ul>
+          {navLinks.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} className="icon-hover" alt={`icon-${id}`} />
+            </li>
+          ))}
+        </ul>
+
+        <time>{dayjs().format("ddd MMM D H:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
